dc3bd01efd15afa2d09975a45aba0e6c1e17f6f1
Nathaniel Mutkus <mutkus@colorado.edu>
2023-03-29 00:26:02 +0000

    Added db_test route

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/db_test')
+def testing():
+    conn = psycopg2.connect("postgres://mutkus_lab_10_user:9JkJGkXZM0UhCs0ma5fpOGWJMDYwrxan@dpg-cgho5bg2qv2772n3qet0-a/mutkus_lab_10")
+    conn.close()
+    return "Database Connection Successful"
